77345e88688b1c5e3da1177bdd6cfc7d0240085e
William Panting <willtp87@hotmail.com>
2011-06-07 13:44:12 -0300

    dynamic templating done?

--- permissions_table.tpl.php
+++ permissions_table.tpl.php
@@ -8,7 +8,7 @@
       <!-- Display a header for each collection -->
       <?php 
         foreach($collections as $collection) {
-          print('<TR>'.$collection.'</TR>');
+          print('<TH>'.key($collections).'</BR>'.$collection.'</TH>');
         }
       ?>
     </TR>
@@ -20,11 +20,10 @@
 	<TBODY>
 	<!-- Display Table cells for each collection/role intersection -->
 		<?php 
-		  foreach($roles as $role) {
-		    print('<TR><TD>'.$role.'</TD>');
-		  
-		    foreach($collections as $collection) {
-		      print('<TD>'.$table_elems.'</TD>');
+		  foreach($roles as $role_id => $role_name) {
+		    print('<TR><TD>'.key($role_name).'</TD>');
+		    foreach($collections as $collection_id) {
+		      print('<TD>'.$list[$role_id][key($collections)].'</TD>');
 		    }
 		    print('</TR>');
 		  }
