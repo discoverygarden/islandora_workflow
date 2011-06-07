38d45b63d4a64ffa7adcd61d9c01b1c82c726016
William Panting <willtp87@hotmail.com>
2011-06-07 13:02:34 -0300

    first draft of dynamic template for perm page

--- permissions_table.tpl.php
+++ permissions_table.tpl.php
@@ -7,9 +7,9 @@
       </TH>
       <!-- Display a header for each collection -->
       <?php 
-        /*foreach($collections as $collection) {
-          print(<TR>$collection</TR>);
-        }*/
+        foreach($collections as $collection) {
+          print('<TR>'.$collection.'</TR>');
+        }
       ?>
     </TR>
 	</THEAD>
@@ -20,9 +20,14 @@
 	<TBODY>
 	<!-- Display Table cells for each collection/role intersection -->
 		<?php 
+		  foreach($roles as $role) {
+		    print('<TR><TD>'.$role.'</TD>');
 		  
-		  print('<TR><TD>'.$table_elems.'</TD></TR>');
-		  
+		    foreach($collections as $collection) {
+		      print('<TD>'.$table_elems.'</TD>');
+		    }
+		    print('</TR>');
+		  }
 		  ?>
 	</TBODY>
 	
