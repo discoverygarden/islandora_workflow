b54796e193f24faa7cc930b1f04e53c9b51618f8
William Panting <willtp87@hotmail.com>
2011-06-16 10:16:07 -0300

    permissions by user now displaying

--- permissions_user_table.tpl.php
+++ permissions_user_table.tpl.php
@@ -20,7 +20,7 @@
 	<TBODY>
 	<!-- Display Table cells for each collection/role intersection -->
 		<?php 
-		  foreach($user as $user_id => $user_name) {
+		  foreach($users as $user_id => $user_name) {
 		    print('<TR><TD>'.key($user_name).'</TD>');
 		    foreach($collections as $collection_id =>$collection_name) {
 		      print('<TD>'.$list[$user_id][$collection_id].'</TD>');
