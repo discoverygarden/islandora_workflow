1d3eae5c44bfc3f556c8f2288c7cb2bcf521fadc
William Panting <willtp87@hotmail.com>
2011-06-16 11:52:03 -0300

    workflow themeing working again

--- workflow_table.tpl.php
+++ workflow_table.tpl.php
@@ -23,7 +23,19 @@
 	</TFOOT>
 	
 	<TBODY>
-	<?php /*
+	<?php 
+	dsm($list);
+	foreach($collections as $collection_id => $collection_name) {
+  $collection_members=get_related_items_as_array($collection_id, 'isMemberOf', 10000 , 0, FALSE);
+    foreach($collection_members as $member) {
+      if (isset($list[$collection_id][$member])) { //The isset is here so that only populated items are displayed on the workflow tab
+        print('<TR>');
+        print('<TD>'.$member.'</TD><TD>'.$collection_name.'</TD><TD></TD><TD></TD><TD>');
+        print($list[$collection_id][$member]);
+        print('</TD></TR>');
+      }
+    }
+  }/*
     foreach ($list as $collection_id => $list_element) {
       if(is_array($list_element)) {
         foreach ($list_element as $object_id => $objects) {
