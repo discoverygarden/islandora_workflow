3318011395148c493a824ffa2d4a26357f24b502
William Panting <willtp87@hotmail.com>
2011-07-11 16:55:37 -0300

    collection_perms page now only showing one collection, which is selectable

--- permissions_role_table.tpl.php
+++ permissions_role_table.tpl.php
@@ -1,3 +1,6 @@
+<?php 
+  print($list['collection_selector'].$list['collection_submit']);
+?>
 <TABLE>
 
   <THEAD>
@@ -7,9 +10,10 @@
       </TH>
       <!-- Display a header for each collection -->
       <?php 
-        foreach($collections as $collection_id => $collection_name) {
+        /*foreach($collections as $collection_id => $collection_name) {
           print('<TH>' . $collection_id .'</BR>' . $collection_name .'</TH>');
-        }
+        }*/
+      print('<TH>' . $_SESSION['workflow_permissions_page']['active_collection'] .'</TH>');
       ?>
     </TR>
 	</THEAD>
@@ -22,9 +26,10 @@
 		<?php 
 		  foreach($roles as $role_id => $role_name) {
 		    print('<TR><TD>'.key($role_name).'</TD>');
-		    foreach($collections as $collection_id =>$collection_name) {
+		    print('<TD>'.$list[$role_id][$_SESSION['workflow_permissions_page']['active_collection']].'</TD>');
+		    /*foreach($collections as $collection_id =>$collection_name) {
 		      print('<TD>'.$list[$role_id][$collection_id].'</TD>');
-		    }
+		    }*/
 		    print('</TR>');
 		  }
 		  ?>
--- permissions_user_table.tpl.php
+++ permissions_user_table.tpl.php
@@ -1,3 +1,6 @@
+<?php 
+  print($list['collection_selector'].$list['collection_submit']);
+?>
 <TABLE>
 
   <THEAD>
@@ -7,9 +10,10 @@
       </TH>
       <!-- Display a header for each collection -->
       <?php 
-        foreach($collections as $collection_id => $collection_name) {
+        /*foreach($collections as $collection_id => $collection_name) {
           print('<TH>' . $collection_id .'</BR>' . $collection_name .'</TH>');
-        }
+        }*/
+      print('<TH>' . $_SESSION['workflow_permissions_page']['active_collection'] .'</TH>');
       ?>
     </TR>
 	</THEAD>
@@ -22,9 +26,10 @@
 		<?php 
 		  foreach($users as $user_id => $user_name) {
 		    print('<TR><TD>'.key($user_name).'</TD>');
-		    foreach($collections as $collection_id =>$collection_name) {
-		      print('<TD>'.$list[$user_id][$collection_id].'</TD>');
-		    }
+		    print('<TD>'.$list[$user_id][$_SESSION['workflow_permissions_page']['active_collection']].'</TD>');
+		    /*foreach($collections as $collection_id =>$collection_name) {
+		      print('<TD>'.$list[$role_id][$collection_id].'</TD>');
+		    }*/
 		    print('</TR>');
 		  }
 		  ?>
