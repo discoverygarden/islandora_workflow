15c30382a263e1dc1a3a1baee490e1f1d7a0930c
William Panting <willtp87@hotmail.com>
2011-08-23 16:30:47 -0300

    colorado debug

--- workflow_table.tpl.php
+++ workflow_table.tpl.php
@@ -33,7 +33,6 @@
 	  
     $collection_members=islandora_workflow_get_all_members_of_collection($collection_id);
     foreach($collection_members as $member) {
-      
       if (isset($list[$collection_id][$member])) { //The isset is here so that only populated items are displayed on the workflow tab
         print('<TR>');
         print('<TD>'. $list[$collection_id][$member]['Selecter'] .'</TD><TD>'.$list[$collection_id][$member]['object'].
