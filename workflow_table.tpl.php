99d88eff4d0770a59b752385db445aad7687c942
William Panting <willtp87@hotmail.com>
2011-08-23 10:54:44 -0300

    init workflow updated, some temparary test changes

--- workflow_table.tpl.php
+++ workflow_table.tpl.php
@@ -30,9 +30,10 @@
 	<TBODY>
 	<?php 
 	foreach($collections as $collection_id => $collection_name) {
-  //$collection_members=get_related_items_as_array($collection_id, 'isMemberOf', 10000 , 0, FALSE);
+	  
     $collection_members=islandora_workflow_get_all_members_of_collection($collection_id);
     foreach($collection_members as $member) {
+      
       if (isset($list[$collection_id][$member])) { //The isset is here so that only populated items are displayed on the workflow tab
         print('<TR>');
         print('<TD>'. $list[$collection_id][$member]['Selecter'] .'</TD><TD>'.$list[$collection_id][$member]['object'].
