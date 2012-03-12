e723d21168c4038f83b57ef1f21b7847100c3347
William Panting <willtp87@hotmail.com>
2012-03-12 09:32:51 -0300

    fixed a pluralization issue in the UI

--- permissions_role_table.tpl.php
+++ permissions_role_table.tpl.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
   print($list['collection_selector'].$list['collection_submit']);
 ?>
 <TABLE>
@@ -6,21 +6,21 @@
   <THEAD>
     <TR>
       <TH>
-      		Roles
+      		Role
       </TH>
       <!-- Display a header for each collection -->
-      <?php 
+      <?php
       print('<TH>' . $_SESSION['workflow_permissions_page']['active_collection'] .'</TH>');
       ?>
     </TR>
 	</THEAD>
-	
+
 	<TFOOT>
 	</TFOOT>
-	
+
 	<TBODY>
 	<!-- Display Table cells for each collection/role intersection -->
-		<?php 
+		<?php
 		  foreach($roles as $role_id => $role_name) {
 		    print('<TR><TD>'.key($role_name).'</TD>');
 		    print('<TD>'.$list[$role_id][$_SESSION['workflow_permissions_page']['active_collection']].'</TD>');
@@ -28,5 +28,5 @@
 		  }
 		  ?>
 	</TBODY>
-	
+
 </TABLE>
