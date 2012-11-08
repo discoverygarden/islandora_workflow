bb97171a9cc9658b3fa4d898ef75a0d36440ea97
unknown <William@William-PC.(none)>
2012-11-08 14:51:46 -0400

    Code style/docs.

--- theme/permissions_user_table.tpl.php
+++ theme/permissions_user_table.tpl.php
@@ -11,30 +11,26 @@
   <THEAD>
     <TR>
       <TH>
-      		User
+        User
       </TH>
       <th>
         <?php print $list['active_collection_label'] . ' [' . $list['active_collection_pid'] . ']'; ?>
       </th>
-      <!-- Display a header for each collection -->
-      <?php
-      //print('<TH>' . $_SESSION['workflow_permissions_page']['active_collection'] . '</TH>');
-      ?>
     </TR>
 	</THEAD>
 
-	<TFOOT>
-	</TFOOT>
+  <TFOOT>
+  </TFOOT>
 
-	<TBODY>
-	<!-- Display Table cells for each collection/role intersection -->
-      <?php
-        foreach($users as $user_id => $user_name) :
-          print('<TR><TD>' . key($user_name) . '</TD>');
-          print('<TD>' . $list[$user_id][$_SESSION['workflow_permissions_page']['active_collection']] . '</TD>');
-          print('</TR>');
-        endforeach;
-      ?>
-	</TBODY>
+  <TBODY>
+    <!-- Display Table cells for each collection/role intersection -->
+    <?php
+      foreach($users as $user_id => $user_name) :
+        print('<TR><TD>' . key($user_name) . '</TD>');
+        print('<TD>' . $list[$user_id][$_SESSION['workflow_permissions_page']['active_collection']] . '</TD>');
+        print('</TR>');
+      endforeach;
+    ?>
+  </TBODY>
 
 </TABLE>
